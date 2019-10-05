8bac18dd59449b03deaacd48b0be4c3f4bdf1b58
jats2k9 <jats2k9@gmail.com>
2019-10-05 00:28:06 -0400

    Initial commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+export default function AuthorQuiz() {
+    return (
+        <div>Author Quiz</div>
+    );
+}
+
